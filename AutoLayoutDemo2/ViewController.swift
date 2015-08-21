d3fec29209a1636f72d0ee2ddda1d1b1bec9d610
fanhl <Dyguest0315>
2015-08-21 10:53:43 +0800

    Initial Commit

---
+++ AutoLayoutDemo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutDemo2
+//
+//  Created by 范鸿麟 on 15/8/21.
+//  Copyright (c) 2015年 富通. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
